5c912d45cfb1d1a8bea9c1556345635e69b46624
王亮 <wangliang@codingapi.com>
2021-08-19 19:01:24 +0800

    application

--- src/main/java/com/codingapi/vscode/springboot/SpringbootDemoApplication.java
+++ src/main/java/com/codingapi/vscode/springboot/SpringbootDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
 }
